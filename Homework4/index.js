04b5dcba220a3279e24ba096c0196c8ee9d84b56
LutsenkoDmitro <dmitriylutsenkoo@gmail.com>
2023-03-25 18:12:36 +0200

    create mathematical operators

---
+++ Homework4/index.js
@@ -0,0 +1,27 @@
+// Сложение +
+const a = 2
+let b = 2
+x = b + a
+console.log(`Результат сложения: ${x}`);
+alert(`${a} + ${b} = ${x}`)
+
+//Вычитание -
+const a = 2
+let b = 2
+x = b - a
+console.log(`Результат вычитания: ${x}`);
+alert(`${a} - ${b} = ${x}`)
+
+//Умножение *
+const a = 2
+let b = 2
+x = b * a
+console.log(`Результат умножения: ${x}`);
+alert(`${a} * ${b} = ${x}`)
+
+//Деление /
+const a = 2
+let b = 2
+x = b / a
+console.log(`Результат деления: ${x}`);
+alert(`${a} / ${b} = ${x}`)
