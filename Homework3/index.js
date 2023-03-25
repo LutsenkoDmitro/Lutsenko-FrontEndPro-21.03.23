2d93b302f3230a9b52d45455e36bc36b335f16e6
LutsenkoDmitro <dmitriylutsenkoo@gmail.com>
2023-03-25 21:39:55 +0200

    create homework № 3

---
+++ Homework3/index.js
@@ -0,0 +1,23 @@
+
+
+// Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація).
+let lastName = prompt("Введіть ваше прізвище")
+let firstName = prompt("Введіть своє ім'я");
+let fatherName = prompt("Введіть ім'я по-батькові")
+alert(`${lastName} ${firstName} ${fatherName}`)
+
+
+
+// Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.
+const number = `12345`
+console.log(number.split(``).join(` `))
+
+
+// Вивiд для користувача
+let number1 = prompt("Введіть цифру №1")
+let number2 = prompt("Введіть цифру №2")
+let number3 = prompt("Введіть цифру №3")
+let number4 = prompt("Введіть цифру №4")
+let number5 = prompt("Введіть цифру №5")
+alert(`${number1} ${number2} ${number3} ${number4} ${number5}`)
+
