e961f6e91d7b31cb7fc8b6295916c24ee2b9310d
LutsenkoDmitro <dmitriylutsenkoo@gmail.com>
2023-03-29 20:13:32 +0300

    convert hours to minutes

--- Homework6/index.js
+++ Homework6/index.js
@@ -1,5 +1,18 @@
+let hoursStr = prompt("Яку кiлькiсть годин перевести в секунды?:");
 
-let a = prompt("Введите количество часов")
-let b = a * 3600
-alert(b + " " + `Секунд`)
+function hoursToSeconds(hours) {
+    if (typeof hours === "number") {
+        let seconds = hours * 3600;
+        return seconds;
+    } else {
+        return "Введiть число";
+    }
+}
 
+let hours = Number(hoursStr);
+if (isNaN(hours)) {
+    alert("Введiть число");
+} else {
+    let result = hoursToSeconds(hours);
+    alert(`${result} Секунд`);
+}
