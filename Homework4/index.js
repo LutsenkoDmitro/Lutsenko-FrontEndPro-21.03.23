0113ceef13b19b41785d01d61bdd9af8427ce61e
LutsenkoDmitro <dmitriylutsenkoo@gmail.com>
2023-03-29 19:20:47 +0300

    edit calculate

--- Homework4/index.js
+++ Homework4/index.js
@@ -1,26 +1,26 @@
 // Сложение +
-const a = 2
+let a = 2
 let b = 2
 x = b + a
 console.log(`Результат сложения: ${x}`);
 alert(`${a} + ${b} = ${x}`)
 
 //Вычитание -
-const a = 2
+let a = 2
 let b = 2
 x = b - a
 console.log(`Результат вычитания: ${x}`);
 alert(`${a} - ${b} = ${x}`)
 
 //Умножение *
-const a = 2
+let a = 2
 let b = 2
 x = b * a
 console.log(`Результат умножения: ${x}`);
 alert(`${a} * ${b} = ${x}`)
 
 //Деление /
-const a = 2
+let a = 2
 let b = 2
 x = b / a
 console.log(`Результат деления: ${x}`);
