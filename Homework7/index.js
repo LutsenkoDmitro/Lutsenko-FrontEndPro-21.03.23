5b782cc15a4d3a9fdd50702b2d459bdcf9ba1705
LutsenkoDmitro <dmitriylutsenkoo@gmail.com>
2023-03-30 14:00:32 +0300

    edit average

--- Homework7/index.js
+++ Homework7/index.js
@@ -1,14 +1,20 @@
+let num1Str = prompt("Введiть перше число:");
+let num2Str = prompt("Введiть друге число:");
+let num3Str = prompt("Введiть третє число:");
 
-let popularitySum = 0;
-let itemsFound = 0;
-const len = victorianSlang.length;
-let item = null;
-for (let i = 0; i < len; i++) {
-    item = victorianSlang[i];
-    if (item.found) {
-        popularitySum = item.popularity + popularitySum;
-        itemsFound = itemsFound + 1;
+function calculateAverage(num1, num2, num3) {
+    let sum = num1 + num2 + num3;
+    let x = sum / 3;
+    return x;
+}
 
-    }
-    const averagePopularity = popularitySum / itemsFound;
-    console.log("Average popularity:", averagePopularity);
+let num1 = Number(num1Str);
+let num2 = Number(num2Str);
+let num3 = Number(num3Str);
+
+if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+    alert("Помилка: введено не число");
+} else {
+    let x = calculateAverage(num1, num2, num3);
+    alert("Середнє арифметичне: " + x);
+}
